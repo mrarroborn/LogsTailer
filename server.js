2572446187e9e1be8663f1aa533c3c34fa2af8cc
mrarroborn <harshitsai1709@gmail.com>
2021-10-21 05:24:17 -0700

    commit

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ io.on('connection', client => {
         }
     });
 });
-
-server.listen(5000, () => {
-  console.log('Server started on port *:5000');
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+   console.log(`App is running on port ${ PORT }`);
 });
